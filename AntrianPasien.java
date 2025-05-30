fcffc6136bf2a58d38a247a187b510162baaf8f6
eternitvy <crrissant@gmail.com>
2025-05-30 20:31:37 +0700

    p

---
+++ AntrianPasien.java
@@ -0,0 +1,61 @@
+public class AntrianPasien {
+    Pasien[] antrian;
+    int front, rear, size, max;
+
+    public AntrianPasien(int kapasitas) {
+        max = kapasitas;
+        antrian = new Pasien[max];
+        front = rear = -1;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == max;
+    }
+
+    public void tambahAntrian(Pasien pasien) {
+        if (isFull()) {
+            System.out.println("Antrian penuh! Tidak dapat menambahkan pasien");
+            return;
+        }
+        if (isEmpty()) {
+            front = rear = 0;
+        } else {
+            rear++;
+        }
+        antrian[rear] = pasien;
+        size++;
+        System.out.println("Pasien ditambahkan ke antrian.");
+    }
+
+    public Pasien layaniPasien() {
+        if (isEmpty()) {
+            System.out.println("Antrian kosong! Tidak dapat melayani pasien");
+            return null;
+        }
+        Pasien p = antrian[front];
+        front++;
+        size--;
+        return p;
+    }
+
+    public void tampilkanAntrian() {
+        if (isEmpty()) {
+            System.out.println("Antrian kosong! Tidak ada pasien dalam antrian");
+        } else {
+            System.out.println("=== DAFTAR ANTRIAN PASIEN ===");
+            for (int i = front; i <= rear; i++) {
+                System.out.println((i - front + 1) + ". " + antrian[i].nama + " (NIK: " + antrian[i].nik + ")");
+                System.out.println("   Keluhan: " + antrian[i].keluhan);
+            }
+        }
+    }
+
+    public int sisaAntrian() {
+        return size;
+    }
+}
