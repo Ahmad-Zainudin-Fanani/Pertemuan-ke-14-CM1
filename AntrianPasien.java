c3a154d0657cba6e3455e32de651e5e9b1776b08
Adhityayusuf <adhitya.yusuf1206@gmail.com>
2025-06-03 10:57:47 +0700

    p

--- AntrianPasien.java
+++ AntrianPasien.java
@@ -5,7 +5,7 @@ public class AntrianPasien {
     public AntrianPasien(int kapasitas) {
         max = kapasitas;
         antrian = new Pasien[max];
-        front = rear = -1;
+        front = rear = 0;
         size = 0;
     }
 
@@ -19,43 +19,44 @@ public class AntrianPasien {
 
     public void tambahAntrian(Pasien pasien) {
         if (isFull()) {
-            System.out.println("Antrian penuh!");
+            System.out.println(">> Antrian penuh!");
             return;
         }
-        if (isEmpty()) {
-            front = rear = 0;
-        } 
-        else {
-            rear++;
-        }
+
         antrian[rear] = pasien;
+        rear = (rear + 1) % max;  // perputaran indeks
         size++;
+
         System.out.println(">> Pasien masuk ke dalam antrian.");
     }
 
     public Pasien layaniPasien() {
         if (isEmpty()) {
-            System.out.println("Antrian kosong!");
+            System.out.println(">> Antrian kosong!");
             return null;
         }
+
         Pasien dilayani = antrian[front];
-        front++;
+        front = (front + 1) % max;  // maju secara melingkar
         size--;
+
         return dilayani;
     }
 
     public void tampilkanAntrian() {
         if (isEmpty()) {
             System.out.println(">> Tidak ada pasien dalam antrian.");
-        } 
-        else {
+        } else {
             System.out.println("== Antrian Pasien ==\n");
-            for (int i = front; i <= rear; i++) {
-                antrian[i].tampilkanInformasi();
+            int index = front;
+            for (int i = 0; i < size; i++) {
+                antrian[index].tampilkanInformasi();
                 System.out.println("--------------------------");
+                index = (index + 1) % max;
             }
         }
     }
+
 
     public int sisaAntrian() {
         return size;
--- RiwayatTransaksi.java
+++ RiwayatTransaksi.java
@@ -22,35 +22,9 @@ public class RiwayatTransaksi {
         }
         NodeTransaksi temp = head;
         System.out.println("== Riwayat Transaksi ==\n");
-        // System.out.println("Daftar Transaksi :");
         while (temp != null) {
             temp.data.tampilkanTransaksi();
             temp = temp.next;
-        }
-    }
-
-    public void removeFirst() {
-        if (isEmpty()) {
-            System.out.println("Linked list kosong!");
-        } else if (head == tail) {
-            head = tail = null;
-        } else {
-            head = head.next;
-        }
-    }
-
-    public void removeLast() {
-        if (isEmpty()) {
-            System.out.println("Linked list kosong!");
-        } else if (head == tail) {
-            head = tail = null;
-        } else {
-            NodeTransaksi temp = head;
-            while (temp.next != tail) {
-                temp = temp.next;
-            }
-            temp.next = null;
-            tail = temp;
         }
     }
 }
